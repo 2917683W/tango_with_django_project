22a35e0618029190399a76b85caabb40c2830427
Filipe Wang <2917683w@student.gla.ac.uk>
2026-02-18 02:56:11 +0000

    Chapter 4 - Passed context data into template

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# def index(request):
+#    return HttpResponse("Rango says hello world")
+
+#def index(request):
+#    return render(request, 'rango/index.html')
+
+
 def index(request):
-    return render(request, 'rango/index.html')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
